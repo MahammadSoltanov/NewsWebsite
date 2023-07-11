12e1f9e1cccf5d53c2aa41af7819009b264a0831
MaqaSoltanov <113199901+MaqaSoltanov@users.noreply.github.com>
2023-07-11 16:20:12 +0400

    Defined manual validation for adding Language

--- Presentation/Pages/Admin/Lan/AddLanguage.cshtml.cs
+++ Presentation/Pages/Admin/Lan/AddLanguage.cshtml.cs
@@ -1,28 +1,35 @@
 using Application.CQRS.Languages.Commands.CreateLanguage;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Results;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Presentation.Pages.Admin.Lan
 {
     public class AddLanguageModel : PageModel
     {
+        private readonly IMediator _mediator;
+        private readonly IValidator<CreateLanguageCommand> _validator;
+
+        public AddLanguageModel(IMediator mediator, IValidator<CreateLanguageCommand> validator)
+        {
+            _mediator = mediator;
+            _validator = validator;
+        }
+
         [BindProperty]
         public string Title { get; init; }
         [BindProperty]
         public string Code { get; init; }
-        private readonly IMediator _mediator;
-        public AddLanguageModel(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+
         public void OnGet()
         {
 
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<ActionResult> OnPostAsync()
         {
             CreateLanguageCommand createLanguageCommand = new CreateLanguageCommand()
             {
@@ -30,9 +37,19 @@ namespace Presentation.Pages.Admin.Lan
                 Title = Title
             };
 
+            ValidationResult result = await _validator.ValidateAsync(createLanguageCommand);
+
+            if(!result.IsValid) 
+            {
+                result.AddToModelState(this.ModelState);
+
+                return Page();
+            }
+
+
             int id = await _mediator.Send(createLanguageCommand);
             string _message = $"Language with ID = {id} was successfully created";
-            return new RedirectToPageResult("/Admin/Succeed", new { message = _message, entityName = "Language" });
+            return RedirectToPage("/Admin/Succeed", new { message = _message, entityName = "Language" });
         }
     }
 }
