538b808d66032f10d471c0345ac3ac537909afda
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-12-23 16:39:16 +0400

    refactor: make navigation component in admin panel reusable with ViewComponents and Views

---
+++ Presentation/Models/Navigation/AdminMenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentation.Models.Navigation;
+public sealed class AdminMenuItem
+{
+    public string Title { get; init; }
+    public string Url { get; init; }
+    public string IconHtml { get; init; }
+    public Func<bool> IsVisible { get; init; }
+}
+
---
+++ Presentation/ViewComponents/AdminSideBarViewComponent.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.Models.Navigation;
+
+namespace Presentation.ViewComponents;
+
+public class AdminSidebarViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(bool isAdmin, bool isModerator)
+    {
+        var items = new List<AdminMenuItem>
+        {
+            new()
+            {
+                Title = "Languages",
+                Url = "/Admin/Languages",
+                IconHtml = "<ion-icon class='nav-icon far' name='language-outline'></ion-icon>",
+                IsVisible = () => isAdmin
+            },
+            new()
+            {
+                Title = "Posts",
+                Url = "/Admin/Posts",
+                IconHtml = "<i class='nav-icon far fa-image'></i>",
+                IsVisible = () => true
+            },
+            new()
+            {
+                Title = "Users",
+                Url = "/Admin/Users",
+                IconHtml = "<i class='nav-icon fas ion-person-add'></i>",
+                IsVisible = () => isAdmin || isModerator
+            },
+            new()
+            {
+                Title = "Hashtags",
+                Url = "/Admin/Hashtags",
+                IconHtml = "<ion-icon class='nav-icon fas' name='pricetag-outline'></ion-icon>",
+                IsVisible = () => true
+            },
+            new()
+            {
+                Title = "Categories",
+                Url = "/Admin/Categories",
+                IconHtml = "<ion-icon class='nav-icon fas' name='grid-outline'></ion-icon>",
+                IsVisible = () => true
+            },
+            new()
+            {
+                Title = "Category approval",
+                Url = "/Admin/Approve/CategoryTranslationsStatus",
+                IconHtml = "<ion-icon class='nav-icon fas' name='checkmark-done-outline'></ion-icon>",
+                IsVisible = () => isAdmin || isModerator
+            },
+            new()
+            {
+                Title = "Post approval",
+                Url = "/Admin/Approve/PostsStatus",
+                IconHtml = "<ion-icon class='nav-icon fas' name='checkmark-done-outline'></ion-icon>",
+                IsVisible = () => true
+            },
+            new()
+            {
+                Title = "Post translation approval",
+                Url = "/Admin/Approve/PostTranslationsStatus",
+                IconHtml = "<ion-icon class='nav-icon fas' name='checkmark-done-outline'></ion-icon>",
+                IsVisible = () => true
+            },
+
+        };
+
+        return View(items.Where(x => x.IsVisible()));
+    }
+}
